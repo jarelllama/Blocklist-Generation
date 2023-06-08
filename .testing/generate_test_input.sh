70afef0c88f4b8799cc3783a15215cc88d548689
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-06-08 03:47:26 +0000

    Update

--- .testing/generate_test_input.sh
+++ .testing/generate_test_input.sh
@@ -5,17 +5,12 @@ wget -q https://tranco-list.eu/top-1m.csv.zip
 unzip top-1m.csv.zip
 head -n 2000 top-1m.csv
     | cut -d ',' -f 2 \
-    | sed 's/\r//' > top2000.txt
+    | sed 's/\r//' \
+    > top2000.txt
 
-# Extract the top 2000 domains from DomCop TOP10M
-get -q https://www.domcop.com/files/top/top10milliondomains.csv.zip
-unzip top10milliondomains.csv.zip
-tail -n +2 top10milliondomains.csv \
-    | head  -n 2000 \
-    | cut -d ',' -f 2 \
-    | awk '{gsub(/"/ ,""); print}' >> top2000.txt
-
-# Collate the two to get the top 2000 domains list
-sort -u top2000.txt \
-    | head -n 2000 > top2000.tmp
-mv top2000.tmp top2000.txt
+# Extract 2000 random domains from Hagezi's TIF Light blocklist
+wget -q https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/tif.light.txt
+grep -v '^#' tif.light.txt > tif.light.tmp
+shuf tif.light.tmp \
+    | head -n 2000 \
+    > malicious.txt
