ee16f529eb97196b8c79d56ce98da7812831ed7a
Jarell <91372088+jarelllama@users.noreply.github.com>
2023-07-02 05:07:58 +0000

    placeholder

--- scam-blocklist/retrieve.sh
+++ scam-blocklist/retrieve.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-cleanup() {
-    find . -maxdepth 1 -type f -name "*.tmp" -delete
-}
+#cleanup() {
+#    find . -maxdepth 1 -type f -name "*.tmp" -delete
+#}
 
-trap cleanup exit
+#trap cleanup EXIT
 
-retrieveDomains() {
+retrieve_domains() {
+    search_id="427244e9065f64c3f"
+    #TODO: use secret for this
+    api_key=
+    #TODO: use a search terms file
+    encoded_term="We%20have%20spent%20our%20entire%20lives%20in%20the%20business%20of%20clothing%2C%20from%20economy%20lines%20to%20luxury%20lines%2C.Thousands%20of%20products%20in%20different%20styles%20are%20waiting%20for%20you!After%20spending%2015%20years%20learning%20this%20market%20and%20business%20we%20decided%20to%20put%20all%20of%20our%20contacts%20in%20manufacturing%20and%20designing%20to%20good%20use.%20Our%20mission%20is%20to%20bring%20the%20newest%20and%20best%20designs%20in%20clothing%20to%20you."
+
     # TODO: output all pages
-    curl 'https://customsearch.googleapis.com/customsearch/v1?cx=427244e9065f64c3f&exactTerms=We%20have%20spent%20our%20entire%20lives%20in%20the%20business%20of%20clothing%2C%20from%20economy%20lines%20to%20luxury%20lines%2C.Thousands%20of%20products%20in%20different%20styles%20are%20waiting%20for%20you!After%20spending%2015%20years%20learning%20this%20market%20and%20business%20we%20decided%20to%20put%20all%20of%20our%20contacts%20in%20manufacturing%20and%20designing%20to%20good%20use.%20Our%20mission%20is%20to%20bring%20the%20newest%20and%20best%20designs%20in%20clothing%20to%20you.&filter=0&key=
+    curl \
+        'https://customsearch.googleapis.com/customsearch/v1?cx="$search_id"&exactTerms="$search_term"&filter=0&key="$api_key"
 }
 
